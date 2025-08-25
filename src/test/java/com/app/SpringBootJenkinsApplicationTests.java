b2bf437e9ac60e143dd4349de0e4cea9519a1695
Malleswaramma <malleswarammaulli@gmail.com>
2025-08-25 23:34:40 +0530

    third commit

--- src/test/java/com/app/SpringBootJenkinsApplicationTests.java
+++ src/test/java/com/app/SpringBootJenkinsApplicationTests.java
@@ -1,13 +1,23 @@
 package com.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootJenkinsApplicationTests {
 
+	Logger logger = LoggerFactory.getLogger(SpringBootJenkinsApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		logger.info("test case executing....");
+
+		assertEquals(true, true);
+		logger.info("test case execution done ....");
 	}
 
 }
